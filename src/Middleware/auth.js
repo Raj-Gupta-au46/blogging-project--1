e035863d0d71fecaa0fd6e62ac3a6035fd956379
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2023-11-27 00:32:35 +0530

    review code done

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -1,64 +1,59 @@
-const { response } = require('express')
-const jwt = require('jsonwebtoken')
-const newblog = require('../model/newblog')
+const { response } = require("express");
+const jwt = require("jsonwebtoken");
+const newblog = require("../model/newblog");
 
 const authentication = async function (req, res, next) {
-    try {
-        let token = req.headers["x-api-key"]
-        if (!token) {
-            return res.status(400).send({ status: false, msg: "token is required" })
-        }
-        let decodedToken = jwt.verify(token, "BloggingProject-01")
-        if (!decodedToken) {
-            return res.status(401).send({ msg: "request denied" })
-        }
-        req["x-api-key"] = decodedToken
-
-        next()
+  try {
+    let token = req.headers["x-api-key"];
+    if (!token) {
+      return res.status(400).send({ status: false, msg: "token is required" });
     }
-    catch (err) {
-        return res.status(500).send({ msg: err.message })
+    let decodedToken = jwt.verify(token, "BloggingProject-01");
+    if (!decodedToken) {
+      return res.status(401).send({ msg: "request denied" });
     }
-}
+    req["x-api-key"] = decodedToken;
 
-const autherisatioin = async function (req, res, next) {
+    next();
+  } catch (err) {
+    return res.status(500).send({ msg: err.message });
+  }
+};
 
-    let authorId = req["x-api-key"].authorId
+const authorization = async function (req, res, next) {
+  let authorId = req["x-api-key"].authorId;
 
-    console.log(authorId)
+  console.log(authorId);
 
-    let authoridFromQuery = req.query.blogId
-    let blogId = req.params.blogId
-    console.log(blogId)
-    console.log(authoridFromQuery)
+  let authoridFromQuery = req.query.blogId;
+  let blogId = req.params.blogId;
+  console.log(blogId);
+  console.log(authoridFromQuery);
 
-    if (blogId) {
-        let blog = await newblog.findById(blogId)
-        if (!blog) {
-            return res.status(404).send({ status: false, msg: "blog not found" })
-        }
-        if (blog.authorId != authorId) {
-            return res.status(401).send({ status: false, msg: "unauthorised" })
-        } else {
-            next()
-        }
-
+  if (blogId) {
+    let blog = await newblog.findById(blogId);
+    if (!blog) {
+      return res.status(404).send({ status: false, msg: "blog not found" });
     }
-
-    if (authoridFromQuery) {
-        let blog = await newblog.findById(authoridFromQuery)
-        if (!blog) {
-            return res.status(404).send({ status: false, msg: "blog not found" })
-        }
-        if (blog.authorId != authorId) {
-            return res.status(401).send({ status: false, msg: "unauthorised" })
-        } else {
-            next()
-        }
-
+    if (blog.authorId != authorId) {
+      return res.status(401).send({ status: false, msg: "unauthorised" });
+    } else {
+      next();
     }
-}
+  }
 
+  if (authoridFromQuery) {
+    let blog = await newblog.findById(authoridFromQuery);
+    if (!blog) {
+      return res.status(404).send({ status: false, msg: "blog not found" });
+    }
+    if (blog.authorId != authorId) {
+      return res.status(401).send({ status: false, msg: "unauthorised" });
+    } else {
+      next();
+    }
+  }
+};
 
-module.exports.authentication = authentication
-module.exports.autherisatioin = autherisatioin
+module.exports.authentication = authentication;
+module.exports.authorization = authorization;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,31 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const authermodel = require("../model/newBlogauthor")
-const blogmodel = require("../model/newblog")
-const controller = require("../controller/autherontroller")
-const middleware = require("../Middleware/auth")
+const authermodel = require("../model/newBlogauthor");
+const blogmodel = require("../model/newblog");
+const controller = require("../controller/autherontroller");
+const middleware = require("../Middleware/auth");
 
-router.post("/authors", controller.authors)
-router.post("/blogs", middleware.authentication, controller.blogs)
-router.get("/blogs", middleware.authentication, controller.getblogs)
-router.put("/blogs/:blogId", middleware.authentication, middleware.autherisatioin, controller.updateblogs)
-router.delete("/blogs/:blogId", middleware.authentication, middleware.autherisatioin, controller.deletebyparams)
-router.delete("/blogs", middleware.authentication, middleware.autherisatioin, controller.delBySpecificField)
-router.post("/login", controller.login)
-
-
+router.post("/authors", controller.authors);
+router.post("/blogs", middleware.authentication, controller.blogs);
+router.get("/blogs", middleware.authentication, controller.getblogs);
+router.put(
+  "/blogs/:blogId",
+  middleware.authentication,
+  middleware.authorization,
+  controller.updateblogs
+);
+router.delete(
+  "/blogs/:blogId",
+  middleware.authentication,
+  middleware.authorization,
+  controller.deletebyparams
+);
+router.delete(
+  "/blogs",
+  middleware.authentication,
+  middleware.autherisatioin,
+  controller.delBySpecificField
+);
+router.post("/login", controller.login);
 
 module.exports = router;
